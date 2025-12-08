8d0471560ef8bacb37814ed0fd67ab158f1a5e5d
Maxime Pertici <mpertici@beapi.fr>
2025-12-08 16:31:33 +0100

    Use choices on post_parent_js select

--- ressources/admin.js
+++ ressources/admin.js
@@ -1,4 +1,8 @@
 jQuery(document).ready(function () {
+
+    // Choices.js instance - will be initialized after AJAX load
+    let translationSelect = null;
+
     // Listen change on select AJAX, and clone value on no-js select
     // Todo, manage case if value not exist, from a DB change from AJAX and HTML values
     jQuery('select#post_parent_js').on('change', function () {
@@ -17,21 +21,31 @@ jQuery(document).ready(function () {
     jQuery('select#language_translation').on('change', function () {
         checkUnicityTranslation(jQuery(this).val());
     });
+
+    function loadPostParentAjax(post_type, current_value) {
+        jQuery("select#post_parent_js").load(ajaxurl, {
+            'action': 'load_original_content',
+            'post_type': post_type,
+            'current_value': current_value
+        }, function (response, status, xhr) {
+            if (status == "error") {
+                alert("Sorry but an error occured with AJAX method");
+            } else {
+                // Destroy previous Choices instance if exists
+                if (translationSelect !== null) {
+                    translationSelect.destroy();
+                }
+                // Initialize Choices.js after options are loaded
+                translationSelect = new Choices(jQuery('#post_parent_js')[0], {
+                    searchEnabled: true,
+                    itemSelectText: '',
+                });
+                syncSelectParentBox();
+            }
+        });
+    }
 });
 
-function loadPostParentAjax(post_type, current_value) {
-    jQuery("select#post_parent_js").load(ajaxurl, {
-        'action': 'load_original_content',
-        'post_type': post_type,
-        'current_value': current_value
-    }, function (response, status, xhr) {
-        if (status == "error") {
-            alert("Sorry but an error occured with AJAX method");
-        } else {
-            syncSelectParentBox();
-        }
-    });
-}
 
 function syncSelectParentBox() {
     jQuery("select#parent_id").val(jQuery("select#post_parent_js").val());
